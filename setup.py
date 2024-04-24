1788c20733b05d059f4762f7d9921f8fac54ff5c
ShadowFlameFox <114284784+ShadowFlameFox@users.noreply.github.com>
2024-04-24 17:27:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.3'
-DESCRIPTION = 'Collection of different APIs.'
+VERSION = '0.0.1'
+DESCRIPTION = 'Makes programming with Python easier.'
 
 # Setting up
 setup(
-    name="pythonapikit",
+    name="painlesspy",
     version=VERSION,
     author="ShadowFlameFox",
     author_email="<shadow_flame_fox@web.de>",
@@ -14,7 +14,7 @@ setup(
     long_description="""Coming soon...""",
     packages=find_packages(),
     install_requires=["requests"],
-    keywords=['Python', 'API', 'Weather', 'Collection', "Integration","Wrapper","Client","Request","Endpoint","HTTP","REST","Web Services","Connector","Utility","Toolset","Helper","Manager","Facade","Handler","Assist","Bundle","Kit","JSON"],
+    keywords=['Python', "WeatherAPI"],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
